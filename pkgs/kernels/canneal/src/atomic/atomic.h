8a83c0449a9fcc0be7ea7d666d71878137e50210
namor <ernieworks@163.com>
2025-11-25 11:49:14 +0800

    fix: riscv64 atomic h of canneal

--- pkgs/kernels/canneal/src/atomic/atomic.h
+++ pkgs/kernels/canneal/src/atomic/atomic.h
@@ -3,19 +3,6 @@
 
 #include <stdint.h>
 
-/* Define several macros which we need */
-#ifndef __STRING
-#define __STRING(x)     #x              /* stringify without expanding x */
-#endif
-
-#ifndef __XSTRING
-#define __XSTRING(x)    __STRING(x)     /* expand x, then stringify */
-#endif
-
-#define u_char unsigned char
-#define u_short unsigned short
-#define u_int unsigned int
-#define u_long unsigned long
 
 
 /* Note: The header files were taken from the source of the BSD kernel. More architectures than listed below are supported by BSD. To add another atomic.h for an architecture ${ARCH}, simply copy the atomic.h file which is located in the sys/${ARCH}/include directory of the kernel source tree. You'll also need any files from that directory on which atomic.h depends, and you'll probably have to slightly adapt the files. */
@@ -40,5 +27,23 @@
 #else
 #  error Architecture not supported by atomic.h
 #endif
+
+#ifndef __STRING
+#define __STRING(x)     #x              /* stringify without expanding x */
+#endif
+
+#ifndef __XSTRING
+#define __XSTRING(x)    __STRING(x)     /* expand x, then stringify */
+#endif
+
+#if !(defined(__riscv) && (__riscv_xlen == 64))
+/* Define several macros which we need - ONLY FOR NON-RV64 ARCHS */
+#define u_char unsigned char
+#define u_short unsigned short
+#define u_int unsigned int
+#define u_long unsigned long
+#endif
+
+#endif /* _ATOMIC_H_ */
 
 #endif /* _ATOMIC_H_ */
